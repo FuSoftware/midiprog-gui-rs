d975a4e6a5ed63d42d6e5fc458d8a76bdbd82895
Florent Uguet <florent.uguet@gmail.com>
2021-05-21 12:13:52 +0200

    Updates

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,6 @@ use cstr::cstr;
 use qmetaobject::*;
 use midiprog::*;
 
-qrc!(my_resource,
-    "midiprog/qml" {
-        "qml/main.qml",
-        "qml/ju-2/PG300.qml",
-    },
-);
-
 #[derive(QObject, Default)]
 struct ParameterHandler{
     base: qt_base_class!(trait QObject),
@@ -26,20 +19,21 @@ struct ParameterHandler{
 
 fn main() {
 
+    let cfg = "config/PG300/PG300".to_string();
+
     let mut i = midiprog::lib::interpreter::Interpreter::new();
     i.run_commands_str(&[
         "midiconfig data/midi.json".to_owned(),
-        "config data/ju-2.json".to_owned(),
+        "config config/PG300/PG300.json".to_owned(),
         "synth ju-2".to_owned(),
     ]).unwrap();
 
 
-    my_resource();
     qml_register_type::<ParameterHandler>(cstr!("ParameterHandler"), 1, 0, cstr!("ParameterHandler"));
 
     let mut engine = QmlEngine::new();
 
     // (Here the QML code is inline, but one can also load from a file)
-    engine.load_file("qrc:/midiprog/qml/qml/main.qml".into());
+    engine.load_file("config/PG300/PG300.qml".into());
     engine.exec();
 }
